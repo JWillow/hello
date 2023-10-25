1098dd41dd45bb694024a10aded139db9ebc5a0f
Rémy Letient <remy@zendoc.com>
2023-10-25 15:46:12 +0200

    Infra

--- src/main/java/com/example/hello/web/HelloController.java
+++ src/main/java/com/example/hello/web/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.hello.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${myenv.secret}")
+	private String secret;
+	
+	@Value("${test.property}")
+	private String property;
+	
 	@GetMapping("/")
 	public String index() {
-		return "Coucou de Rémy";
+		return "Coucou de Rémy, secret: [" + secret + "] property value: [" + property + "]";
 	}
 }
